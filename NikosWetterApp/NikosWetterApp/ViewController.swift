35399eae6ed84c70088f3ed4bfc4df8b969325ea
Nikolaos Stivaktakis <niksti@gmx.net>
2016-02-15 16:35:09 +0100

    Design ohne Hintergrundbild

--- NikosWetterApp/NikosWetterApp/ViewController.swift
+++ NikosWetterApp/NikosWetterApp/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var NeuerOrtButton: UIButton!
     @IBOutlet weak var VorherNachherButton: UIButton!
     @IBOutlet weak var BurgerMenuButtton: UIButton!
+    @IBOutlet weak var ZusaetzlichesTextView: UITextView!
     var daten : WetterDatenPaket?
     
 //json["weather"]["description"]
@@ -38,9 +39,23 @@ class ViewController: UIViewController {
 
 
     func DatenLaden() {
-        let Wetterdaten = WetterDatenPaket(IOrt: "Darmstadt", IWetterStatus: "Wolkig", ITemperatur: "4" , IKoordinaten: [50,50])
+        let Wetterdaten = WetterDatenPaket(IOrt: "Darmstadt", IWetterStatus: "Clouds", ITemperatur: "4" , IKoordinaten: [50,50])
         
         daten = Wetterdaten
+    }
+    
+    func getRightImage(zutesten : String) -> String {
+        
+        var filename : String  = "File not found"
+        if zutesten == "Clear" {
+           filename =  "sunny.png"
+        }else if zutesten == "Clouds"{
+            filename = "cloudy.png"
+        }else if zutesten == "Rain"{
+            filename = "rainy.png"
+        }
+        
+        return filename
     }
     
     func DatenAnzeigen(){
@@ -49,6 +64,9 @@ class ViewController: UIViewController {
         OrtLabel.text = daten?.Ort
         StatusLabel.text = daten?.Wetterstatus
         TemperaturLabel.text = (daten?.Temperatur)! + " °C"
+        let Wetterstatus = daten?.Wetterstatus
+        IconImageView.image = UIImage(named: "\(getRightImage(Wetterstatus!))")
+        ZusaetzlichesTextView.text = "Druck: 1000 \n SonnenAufgang: 7:20\n SonnenUntergang: 20:20 \n Windstärke: 10 km/h"
         }else{
             OrtLabel.text = "Fehler Beim Laden"
         }
